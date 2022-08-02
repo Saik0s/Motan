d82e2da74069e02e8a18fb01588ed501a987c16b
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2022-08-02 22:42:34 +0200

    Adds CSV package

--- Motion/DataBaseClient.swift
+++ Motion/DataBaseClient.swift
@@ -6,4 +6,21 @@
 //
 
 import Foundation
+import CodableCSV
 
+struct DataBaseClient {
+  var appendData: (_filename: String, _ data: MotionData) -> Void
+}
+
+extension MotionData: Codable {
+
+}
+
+extension DataBaseClient {
+  static var live: DataBaseClient {
+    let encoder = CSVEncoder {
+      $0.headers = MotionData.CodingKeys.allCases.map { $0.rawValue }
+    }
+    let data = try encoder.encode(value, into: Data.self)
+  }
+}
