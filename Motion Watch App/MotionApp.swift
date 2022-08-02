a31bbaaf8c61daecfb208880aecad61595fbd1cd
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2022-08-02 14:15:00 +0200

    Initial Commit

---
+++ Motion Watch App/MotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionApp.swift
+//  Motion Watch App
+//
+//  Created by Igor Tarasenko on 02/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Motion_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Motion/MotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionApp.swift
+//  Motion
+//
+//  Created by Igor Tarasenko on 02/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
